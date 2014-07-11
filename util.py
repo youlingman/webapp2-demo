44e49ca72aa7b3e5f5f8eae6d8f77467fa1cdc99
youling chan <chenyoucyl@gmail.com>
2014-07-11 10:01:20 +0800

    remove unsue funcs

--- util.py
+++ util.py
@@ -1,38 +1,5 @@
-import redis
-import MySQLdb
-from igor import const
 import webapp2
 from webapp2_extras import sessions, auth
-
-# util function
-def list2Filter(filter_list):
-    filter = {}
-    for elem in filter_list:
-        filter.update({elem: True})
-    return filter
-
-# redis wrapper
-r = redis.StrictRedis(**const.redis)
-
-def redis_get(key):
-    return r.get(key)
-
-def redis_set(key, value):
-    r.set(key, value)
-    #r.bgsave()
-
-# mysql wrapper
-def mysql_query_list(query):
-    c= MySQLdb.connect(**const.mysqldb)
-    cursor = c.cursor(MySQLdb.cursors.DictCursor)
-    try:
-        cursor.execute(query)
-        results = cursor.fetchall()
-        c.close()
-        return results
-    except:
-        c.close()
-        return False
 
 # session for webapp2
 class BaseHandler(webapp2.RequestHandler):              # taken from the webapp2 extrta session example
